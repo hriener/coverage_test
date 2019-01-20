67a7dfdc68088bf1d5fc2c9f16e63f681c97482b
Heinz Riener <heinz.riener@gmail.com>
2019-01-20 13:39:27 +0100

    coverage.

--- coverage.cpp
+++ coverage.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
 
-int main ( int, char *[] )
+void f()
 {
+  std::cout << "function called" << std::endl;
+}
+
+int main ( int argc, char *argv[] )
+{
+  (void)argv;
+
   if ( 0 )
     std::cout << "Not covered." << std::endl;
+
+  if ( argc > 10 )
+  {
+    f();
+  }
 
   std::cout << "Sample text." << std::endl;
 
